00c325e3d5069b3744f58fd33320e33c5ec7a7a9
Kevin Rafferty <audible59@yahoo.com>
2017-02-24 15:45:05 -0800

    Initial commit.

---
+++ KERBluetooth/KERBluetooth.h
@@ -0,0 +1,19 @@
+//
+//  KERBluetooth.h
+//  KERBluetooth
+//
+//  Created by Lindsey Rafferty on 2/24/17.
+//  Copyright © 2017 Quiver Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KERBluetooth.
+FOUNDATION_EXPORT double KERBluetoothVersionNumber;
+
+//! Project version string for KERBluetooth.
+FOUNDATION_EXPORT const unsigned char KERBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KERBluetooth/PublicHeader.h>
+
+
