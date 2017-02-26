577a06bf5489aeedbb4f412c89be2904cbcb631c
Kevin Rafferty <audible59@yahoo.com>
2017-02-25 16:00:57 -0800

    * Created a new Sample Application for testing the Bluetooth framework.   - Added the KERBluetooth Framework as a subproject of the Sample Application.

---
+++ KERBluetoothSampleApplication/KERBluetoothSampleApplication/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  KERBluetoothSampleApplication
+//
+//  Created by Lindsey Rafferty on 2/25/17.
+//  Copyright © 2017 Quiver Coding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
