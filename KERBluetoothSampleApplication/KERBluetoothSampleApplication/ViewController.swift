6ef1f5a0ed98a7f8597592d4bb7674d6b82b2e4f
Kevin Rafferty <audible59@yahoo.com>
2017-02-27 16:36:31 -0800

    * Setup up the KERBluetoothManager Singleton to be a proper Swift style singleton.   - The static keyword will allow the Singleton to be lazy instantiated only once.   - Ensured the use of the private init() { } method so that no other classes can instantiate the Singleton's init() method.

--- KERBluetoothSampleApplication/KERBluetoothSampleApplication/ViewController.swift
+++ KERBluetoothSampleApplication/KERBluetoothSampleApplication/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 import KERBluetoothManager
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, KERBluetoothManagerDelegate {
     
-    // MARK: View Life Cycle
+    // MARK: - IBOutlet Properties -
+    
+    @IBOutlet weak var scanButton: UIButton!
+    
+    // MARK: - View Life Cycle -
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        KERBluetoothManager().scanForPeripherals()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - IBAction Methods -
+    
+    @IBAction func onScanButtonPressed(_ sender: Any) {
+        KERBluetoothManager.instance().scanForPeripherals()
+        
+    }
+    
+    // MARK: - KERBluetoothManager Delegate Methods -
+    
+    func didUpdateState(sender: KERBluetoothManager) {
+        print("State Update - \(sender)")
     }
 }
